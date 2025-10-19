53a2b20d81ccfdc6593b88bb4be9b4d8ea0d58ca
nicolaabimbola <nicolaoriola.abimbola@mail.polimi.it>
2025-10-20 01:19:20 +0200

    Refactor `gradient_descent` to improve parameter naming and remove redundant dataset import; update weight saving logic

--- src/training.py
+++ src/training.py
@@ -1,5 +1,5 @@
 import numpy as np
-from src.data_loader import labels_train, data_train, data_test, labels_test, training_set
+from src.data_loader import labels_train, data_train, data_test, labels_test
 from src.neural_network import init_parameters, train, forward_propagation, calculate_loss, calculate_accuracy
 
 batch_size = 16
@@ -12,27 +12,27 @@ def get_accuracy(predictions, labels):
     return np.sum(predictions == labels) / labels.size
 
 
-def gradient_descent(data_train, labels_train):
+def gradient_descent(data, labels):
     W1, b1, W2, b2 = init_parameters()
 
     epochs = 20
     for epoch in range(epochs):
-        shuffle = np.random.permutation(data_train.shape[1])
-        data_train_shuffle = data_train[:, shuffle]
-        labels_train_shuffle = labels_train[shuffle]
+        shuffle = np.random.permutation(data.shape[1])
+        data_train_shuffle = data[:, shuffle]
+        labels_train_shuffle = labels[shuffle]
 
-        for i in range(0, data_train.shape[1], batch_size):
+        for i in range(0, data.shape[1], batch_size):
             x = data_train_shuffle[:, i: i + batch_size]
             y = labels_train_shuffle[i: i + batch_size]
             W1, b1, W2, b2 = train(W1, b1, W2, b2, x, y)
 
-        _,_,_,current_prediction_train = forward_propagation(data_train, W1, b1, W2, b2)
-        train_accuracy = calculate_accuracy(current_prediction_train, labels_train)
+        _,_,_,current_prediction_train = forward_propagation(data, W1, b1, W2, b2)
+        train_accuracy = calculate_accuracy(current_prediction_train, labels)
 
         _, _, _, current_prediction_test = forward_propagation(data_test, W1, b1, W2, b2)
         test_accuracy = calculate_accuracy(current_prediction_test, labels_test)
 
-        training_loss = calculate_loss(current_prediction_train, labels_train)
+        training_loss = calculate_loss(current_prediction_train, labels)
         test_loss = calculate_loss(current_prediction_test, labels_test)
 
         print(f"Epoch: {epoch + 1}/{epochs} | Train accuracy: {train_accuracy:.4f} | "
@@ -42,8 +42,8 @@ def gradient_descent(data_train, labels_train):
     return W1, b1, W2, b2
 
 
-W1, b1, W2, b2 = gradient_descent(data_train, labels_train)
+W1_final, b1_final, W2_final, b2_final = gradient_descent(data_train, labels_train)
 print("Training completed successfully")
 
-np.savez("../results/weights.npz", W1=W1, b1=b1, W2=W2, b2=b2)
+np.savez("../results/weights.npz", W1=W1_final, b1=b1_final, W2=W2_final, b2=b2_final)
 print("Weights saved successfully")
